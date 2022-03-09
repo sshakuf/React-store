00b8f76246c32858d539a64b3e22c81c0ded8920
sshakuf <sshakuf@gmail.com>
2022-03-09 14:36:17 +0200

    added firebase signin with google

--- src/App.js
+++ src/App.js
@@ -7,23 +7,53 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
+import { auth } from './firebase/firebase.utils';
+
+
+
 const HatsPage = () => (
 <div>
   <h1>HATS page</h1> 
 </div>  
 );
 
-function App() {
-  return (
-    <div>
-    <Header />
-      <Routes>
-        <Route exact path='/' element={<HomePage />} /> 
-        <Route path='/shop' element={<ShopPage/>} />
-        <Route path='/signin' element={<SignInAndSignOutPage/>} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user});
+
+      console.log(user);
+
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+      <Header currentUser={this.state.currentUser} />
+        <Routes>
+          <Route exact path='/' element={<HomePage />} /> 
+          <Route path='/shop' element={<ShopPage/>} />
+          <Route path='/signin' element={<SignInAndSignOutPage/>} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
