c1f6f2e8d381610fa77ea3757e99df8b3e25525f
sshakuf <sshakuf@gmail.com>
2022-03-14 14:59:08 +0200

    multiple item in cart display

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,10 @@ import { CartActionTypes } from './cart.types';
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
+
+export const CartAddItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
+
+
